c07255647c2033d501aba49626356b02617260d7
Prasha <prashagoyal99@gmail.com>
2021-11-18 19:31:32 +0530

    write controller functions for users

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -1,2 +1,70 @@
+// require the "users" model
+const User = require("../models/users.model");
+const bcrypt = require("bcrypt");
+
 // CRUD operations for the "users" model
 
+// to create new user during sign-up
+function creatUser(req, res) {
+  const newUser = req.body;
+
+  // storing the hashed password in the document
+  bcrypt.hash(newUser.password, 10, function (err, hashedPassword) {
+    newUser.password = hashedPassword;
+  });
+
+  // creating a new User document
+  User.create(newUser, function (err) {
+    if (err) res.status(200).send({ success: false, message: err });
+    else
+      res.status(200).send({
+        success: true,
+        message: "Succesfully created new user.",
+        user: newUser,
+      });
+  });
+}
+
+// to login the user
+function loginUser(req, res) {
+  const userEmail = req.body.email;
+  const userPassword = req.body.password;
+
+  // finding if the user exists
+  User.findOne({ email: userEmail }, function (err, foundUser) {
+    if (err) res.status(200).send({ success: false, message: err });
+    else {
+      // if the user exists, check the password entered
+      if (foundUser) {
+        // checking password
+        bcrypt.compare(
+          userPassword,
+          foundUser.password,
+          function (err, result) {
+            // if result===true, i.e., the password is correct
+            if (result)
+              res.status(200).send({
+                success: true,
+                message: "Succesfully signed-in the user.",
+                user: foundUser,
+              });
+            // if the password is incorrect
+            else
+              res
+                .status(200)
+                .send({ success: false, message: "Wrong password." });
+          }
+        );
+      }
+
+      // if the user does not exist
+      else
+        res.status(200).send({
+          success: false,
+          message: "The email is not signed-up yet. Please sign-up first.",
+        });
+    }
+  });
+}
+
+module.exports = { creatUser, loginUser };
