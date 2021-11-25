055620d7514488a8eabbd08f54ddd4ddb39ae9a4
Prasha <prashagoyal99@gmail.com>
2021-11-26 00:53:53 +0530

    add messages in attempt quiz

--- client/src/components/AttemptQuiz.jsx
+++ client/src/components/AttemptQuiz.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Card } from "react-bootstrap";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function AttemptQuiz() {
   // to access the data passed to the route
@@ -13,42 +13,63 @@ function AttemptQuiz() {
         {quiz.quizName}
       </h1>
 
-      {/* render a question card for each question */}
-      {quiz.questions.map((question, qId) => (
-        <Card bg="light" className="mb-3">
-          <Card.Header>Question {qId + 1}</Card.Header>
+      {/* <Card bg="light" className="mb-3">
+        <Card.Body>
+          <Card.Title>Thankyou for attempting the quiz!</Card.Title>
+          <Card.Text>
+            Your response has been recorded. Your score is displayed on your
+            dashboard.
+          </Card.Text>
+          <Link to="/quizzes">Back to Dashboard</Link>
+        </Card.Body>
+      </Card> */}
 
-          <Card.Body>
-            <Card.Title className="mb-3">{question.questionTitle}</Card.Title>
+      {quiz.questions.length === 0 ? (
+        <h4>
+          There are no questions in this quiz yet. Please contact the author.
+        </h4>
+      ) : (
+        <div>
+          {/* render a question card for each question */}
+          {quiz.questions.map((question, qId) => (
+            <Card bg="light" className="mb-3">
+              <Card.Header>Question {qId + 1}</Card.Header>
 
-            <Card.Text>
-              {question.options.map((option, opId) => (
-                <div key={opId} className="mb-2">
-                  <input
-                    type="checkbox"
-                    id={qId + "-" + opId}
-                    className="form-check-input d-inline-block mt-2 me-3"
-                    questionid={qId}
-                    optionid={opId}
-                  />
-                  <label class="" for={qId + "-" + opId}>
-                    {option.optionContent}
-                  </label>
-                </div>
-              ))}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      ))}
+              <Card.Body>
+                <Card.Title className="mb-3">
+                  {question.questionTitle}
+                </Card.Title>
 
-      <div className="text-end my-5">
-        <button
-          className="btn btn-primary btn-md px-5 me-3 btn-blue"
-          type="button"
-        >
-          Submit Quiz
-        </button>
-      </div>
+                <Card.Text>
+                  {question.options.map((option, opId) => (
+                    <div key={opId} className="mb-2">
+                      <input
+                        type="checkbox"
+                        id={qId + "-" + opId}
+                        className="form-check-input d-inline-block mt-2 me-3"
+                        questionid={qId}
+                        optionid={opId}
+                      />
+                      <label class="" for={qId + "-" + opId}>
+                        {option.optionContent}
+                      </label>
+                    </div>
+                  ))}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          ))}
+
+          <div className="text-end my-5">
+            <button
+              className="btn btn-primary btn-md px-5 me-3 btn-blue"
+              type="button"
+            >
+              Submit Quiz
+            </button>
+          </div>
+        </div>
+      )}
     </Container>
   );
 }
